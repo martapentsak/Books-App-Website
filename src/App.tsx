0eed7a4d9bbab644c462bfe607c97b82b2b7bb20
Peter Pentsak <pencak.petro@gmail.com>
2024-09-24 14:13:36 +0300

    Added combineProviders

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,29 @@ import { menuElemenets } from "./constants/textValues";
 import { AuthorProvider } from "./context/authors.tsx";
 import { BooksProvider } from "./context/books.tsx";
 
+import { composeProviders } from "./utils/composeProviders.tsx";
+
 import "./App.css";
 import "./styles/global.scss";
 
+const providers = [
+  { Component: BrowserRouter },
+  { Component: AuthorProvider },
+  { Component: BooksProvider },
+];
+
+const CombinedProviders = composeProviders(providers);
+
 function App() {
   return (
-    <div className="books-app">
-      <BrowserRouter>
-        <AuthorProvider>
-          <BooksProvider>
-            <Menu />
-            <Routes>
-              <Route path={menuElemenets.links.home} element={<HomePage />} />
-            </Routes>
-          </BooksProvider>
-        </AuthorProvider>
-      </BrowserRouter>
-    </div>
+    <CombinedProviders>
+      <div className="books-app">
+        <Menu />
+        <Routes>
+          <Route path={menuElemenets.links.home} element={<HomePage />} />
+        </Routes>
+      </div>
+    </CombinedProviders>
   );
 }
 
