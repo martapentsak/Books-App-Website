27a35bdc9613e297890df60b8c357e330f7e8ca0
Peter Pentsak <pencak.petro@gmail.com>
2024-10-03 14:05:16 +0300

    Add all logic to Page Layout

--- src/pages/PageLayout/index.tsx
+++ src/pages/PageLayout/index.tsx
@@ -1,10 +1,21 @@
 import { useCallback, useMemo, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 import { Card } from "../../components/Card";
 
+import { useAuthors } from "../../context/authors";
+import { useBooks } from "../../context/books";
+
+import { menuElemenets } from "../../constants/textValues";
+
 import { UniversalListItem } from "../../types/UniversalListItem";
 
-import { centuryFromYear } from "../../utils/handleGetCentury";
+import { handleGetGenres } from "../../utils/handleGetGenres";
+import { handleGetNationality } from "../../utils/handleGetNationality";
+import {
+  centuryFromYear,
+  handleGetCentuary,
+} from "../../utils/handleGetCentury";
 
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
@@ -26,22 +37,84 @@ type FilteredValues = {
 };
 
 type Props = {
-  selectorsArray: SelectorsArray[];
   listArray: UniversalListItem[];
   isAuthorCard: boolean;
 };
 
-export const PageLayout = ({
-  selectorsArray,
-  listArray,
-  isAuthorCard,
-}: Props) => {
+export const PageLayout = ({ listArray, isAuthorCard }: Props) => {
   const [searchValue, setSearchValue] = useState<string>("");
   const [filteredValues, setFilteredValues] = useState<FilteredValues>({
     genres: "",
     nationality: "",
     century: "",
   });
+
+  const { authorsList, poetsList } = useAuthors();
+  const { booksList } = useBooks();
+
+  const location = useLocation();
+
+  const selectors = [
+    {
+      link: menuElemenets.links.authors,
+      selector: [
+        {
+          label: "Genres",
+          name: "genres",
+          options: handleGetGenres(authorsList),
+        },
+        {
+          label: "Nationality",
+          name: "nationality",
+          options: handleGetNationality(authorsList),
+        },
+      ],
+    },
+    {
+      link: menuElemenets.links.poets,
+      selector: [
+        {
+          label: "Genres",
+          name: "genres",
+          options: handleGetGenres(poetsList),
+        },
+        {
+          label: "Nationality",
+          name: "nationality",
+          options: handleGetNationality(poetsList),
+        },
+      ],
+    },
+    {
+      link: menuElemenets.links.bookStore,
+      selector: [
+        {
+          label: "Genres",
+          name: "genres",
+          options: handleGetGenres(booksList),
+        },
+        {
+          label: "Century",
+          name: "century",
+          options: handleGetCentuary(booksList),
+        },
+      ],
+    },
+    {
+      authorPage: [
+        {
+          label: "Genres",
+          name: "genres",
+          options: handleGetGenres(authorsList),
+        },
+        {
+          label: "Nationality",
+          name: "nationality",
+          options: handleGetNationality(authorsList),
+        },
+      ],
+    },
+  ];
 
   const handleSearchChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => setSearchValue(e.target.value),
@@ -80,6 +153,8 @@ export const PageLayout = ({
     return newFileredArray;
   }, [searchValue, filteredValues, listArray]);
 
+  const currentSelector = selectors.find((v) => v.link === location.pathname);
+
   return (
     <div className="page-layout">
       <div className="page-layout-wrapper">
@@ -98,34 +173,43 @@ export const PageLayout = ({
               },
             }}
           />
-          {selectorsArray.map(
-            ({ label, name, options }: SelectorsArray, index) => (
-              <FormControl key={index}>
-                <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                  {label}
-                </InputLabel>
-                <NativeSelect
-                  defaultValue={options[0]}
-                  onChange={(e) =>
-                    handleFilterValueChange(name, e.target.value)
-                  }
-                >
-                  {options.map((value, index) => (
-                    <option value={value} key={index}>
-                      {value}
-                    </option>
-                  ))}
-                </NativeSelect>
-              </FormControl>
-            )
-          )}
+          {currentSelector && currentSelector.selector
+            ? currentSelector.selector.map(
+                ({ label, name, options }: SelectorsArray, index) => (
+                  <FormControl key={index}>
+                    <InputLabel
+                      variant="standard"
+                      htmlFor="uncontrolled-native"
+                    >
+                      {label}
+                    </InputLabel>
+                    <NativeSelect
+                      defaultValue={options[0]}
+                      onChange={(e) =>
+                        handleFilterValueChange(name, e.target.value)
+                      }
+                    >
+                      {options.map((value, optionIndex) => (
+                        <option value={value} key={optionIndex}>
+                          {value}
+                        </option>
+                      ))}
+                    </NativeSelect>
+                  </FormControl>
+                )
+              )
+            : null}
         </div>
         <div className="list-section">
-          {filteredArray.map((attributes, index) => (
-            <div className="item">
-              <Card {...attributes} key={index} isAuthorCard={isAuthorCard} />
-            </div>
-          ))}
+          {filteredArray.length > 0 ? (
+            filteredArray.map((attributes, index) => (
+              <div className="item" key={index}>
+                <Card {...attributes} isAuthorCard={isAuthorCard} />
+              </div>
+            ))
+          ) : (
+            <div className="no-found">No result....</div>
+          )}
         </div>
       </div>
     </div>
--- src/pages/Routes/index.tsx
+++ src/pages/Routes/index.tsx
@@ -11,52 +11,9 @@ import { menuElemenets } from "../../constants/textValues";
 import { useAuthors } from "../../context/authors";
 import { useBooks } from "../../context/books";
 
-import { handleGetGenres } from "../../utils/handleGetGenres";
-import { handleGetNationality } from "../../utils/handleGetNationality";
-import { handleGetCentuary } from "../../utils/handleGetCentury";
-
 export const AllRoutes = () => {
   const { loading: authorLoading, authorsList, poetsList } = useAuthors();
   const { loading: booksLoading, booksList } = useBooks();
-
-  const selectors = {
-    authorPage: [
-      {
-        label: "Genres",
-        name: "genres",
-        options: handleGetGenres(authorsList),
-      },
-      {
-        label: "Nationality",
-        name: "nationality",
-        options: handleGetNationality(authorsList),
-      },
-    ],
-    poetPage: [
-      {
-        label: "Genres",
-        name: "genres",
-        options: handleGetGenres(poetsList),
-      },
-      {
-        label: "Nationality",
-        name: "nationality",
-        options: handleGetNationality(poetsList),
-      },
-    ],
-    booksPage: [
-      {
-        label: "Genres",
-        name: "genres",
-        options: handleGetGenres(booksList),
-      },
-      {
-        label: "Century",
-        name: "century",
-        options: handleGetCentuary(booksList),
-      },
-    ],
-  };
 
   return (
     <div className="books-app">
@@ -69,32 +26,16 @@ export const AllRoutes = () => {
             <Route path={menuElemenets.links.home} element={<HomePage />} />
             <Route
               path={menuElemenets.links.authors}
-              element={
-                <PageLayout
-                  selectorsArray={selectors.authorPage}
-                  listArray={authorsList}
-                  isAuthorCard={true}
-                />
-              }
+              element={<PageLayout listArray={authorsList} isAuthorCard />}
             />
             <Route
               path={menuElemenets.links.poets}
-              element={
-                <PageLayout
-                  selectorsArray={selectors.authorPage}
-                  listArray={poetsList}
-                  isAuthorCard={true}
-                />
-              }
+              element={<PageLayout listArray={poetsList} isAuthorCard />}
             />
             <Route
               path={menuElemenets.links.bookStore}
               element={
-                <PageLayout
-                  selectorsArray={selectors.booksPage}
-                  listArray={booksList}
-                  isAuthorCard={false}
-                />
+                <PageLayout listArray={booksList} isAuthorCard={false} />
               }
             />
           </Routes>
