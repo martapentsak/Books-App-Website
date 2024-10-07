7aba22bfdf383b3fc9cbab676dc694c6143b88bc
Peter Pentsak <pencak.petro@gmail.com>
2024-10-07 12:40:51 +0300

    Deleted error from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,10 @@ import "./App.css";
 import "./styles/global.scss";
 
 function App() {
-  const { authorLoading, authorListError } = useAuthors();
-  const { booksLoading, bookListError } = useBooks();
+  const { authorLoading } = useAuthors();
+  const { booksLoading } = useBooks();
 
   const isLoading = authorLoading || booksLoading;
-  const error = authorListError ? authorListError : bookListError;
-
-  if (error) {
-    throw new Error(error);
-  }
   return (
     <div className="books-app">
       {isLoading ? <LoadingPage /> : <AllRoutes />}
