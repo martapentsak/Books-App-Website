71eaa57f41518b449d24813ff25f69948e7d64cc
Peter Pentsak <pencak.petro@gmail.com>
2024-10-09 11:49:15 +0300

    Created BookList component

---
+++ src/components/BooksList/index.tsx
@@ -0,0 +1,27 @@
+import { Card } from "../Card";
+
+type CardProps = {
+  id: string;
+  image: string;
+  title?: string;
+  genres: string[];
+  author: string;
+};
+
+type Props = {
+  title: string;
+  data: CardProps[];
+};
+
+export const BooksList = ({ title, data }: Props) => {
+  return (
+    <div className="list-section">
+      <h2 className="list-section-title ">{title}</h2>
+      <div className="flex-wrap-list">
+        {data.map((attributes, index) => (
+          <Card key={index} {...attributes} className="book-card" />
+        ))}
+      </div>
+    </div>
+  );
+};
