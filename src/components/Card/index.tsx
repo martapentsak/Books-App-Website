1656251dacb3bfbdf29102a4fd0dd7c7d6cd472e
Peter Pentsak <pencak.petro@gmail.com>
2024-10-14 11:28:09 +0300

    Changed Card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,20 +1,42 @@
 import CardElement from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardActionArea from "@mui/material/CardActionArea";
-import { ReactElement } from "react";
 
 type Props = {
   className: string;
+  image: string;
+  subtitle?: string;
+  name: string;
+  cardList: string[];
   onClick: () => void;
-  children: ReactElement;
 };
 
-export const Card = ({ className, onClick, children }: Props) => {
+export const Card = ({
+  className,
+  image,
+  onClick,
+  subtitle,
+  name,
+  cardList,
+}: Props) => {
   return (
     <div className={className}>
       <CardElement onClick={onClick}>
         <CardActionArea>
-          <CardContent>{children}</CardContent>
+          <CardContent>
+            <div>
+              <img src={image} alt={"card image"} className={"card-image"} />
+
+              {subtitle && <span className="card-subtitle">{subtitle}</span>}
+              <h2 className="card-name">{name}</h2>
+              {cardList.map((g, index) => (
+                <span className="card-list-item" key={index}>
+                  {g}
+                  {index < cardList.length - 1 ? ", " : ""}
+                </span>
+              ))}
+            </div>
+          </CardContent>
         </CardActionArea>
       </CardElement>
     </div>
