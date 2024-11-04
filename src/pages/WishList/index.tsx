952d7879ce031a0fe9e371343a28c20005b5173c
Peter Pentsak <pencak.petro@gmail.com>
2024-11-04 10:54:03 +0200

    Add widhlist element component

--- src/pages/WishList/index.tsx
+++ src/pages/WishList/index.tsx
@@ -2,22 +2,16 @@ import { useNavigate } from "react-router-dom";
 
 import { Loading } from "../../components/Loading";
 import { Alert } from "../../components/Alert";
+import { WishListElement } from "../../components/WishListElement";
 
 import { useWishlist } from "../../context/wishlist";
 
 import { routes, wishListPage } from "../../constants/textValues";
 
-import DeleteIcon from "@mui/icons-material/Delete";
 import { Button } from "@mui/material";
 
 export const WishList = () => {
-  const {
-    wishList,
-    handleRemoveBookFromWishlist,
-    loading,
-    error,
-    handleCloseWishlistError,
-  } = useWishlist();
+  const { wishList, loading, error, handleCloseWishlistError } = useWishlist();
   const navigate = useNavigate();
 
   if (loading) {
@@ -29,45 +23,9 @@ export const WishList = () => {
       {error && <Alert error={error} onClose={handleCloseWishlistError} />}
       <div className="wishlist-wrapper">
         {wishList.length ? (
-          wishList.map(
-            ({
-              image,
-              title,
-              author,
-              genres,
-              description,
-              publicationYear,
-              id,
-            }) => (
-              <div className="wishlist-element">
-                <div
-                  className="wishlist-info-section"
-                  onClick={() => navigate(`/book/${id}`)}
-                >
-                  <img src={image} className="wishlist-image" />
-                  <div className="wishlist-info">
-                    <h3 className="wishlist-name">{title}</h3>
-                    <h4 className="wishlist-author">{author}</h4>
-                    <h4 className="wishlist-year">{publicationYear}</h4>
-                    <p className="wishlist-description">{description}</p>
-                    {genres.map((g, index) => (
-                      <span className="card-list-item" key={index}>
-                        {g}
-                        {index < genres.length - 1 ? ", " : ""}
-                      </span>
-                    ))}
-                  </div>
-                </div>
-
-                <i
-                  className="delete-btn"
-                  onClick={() => handleRemoveBookFromWishlist(id)}
-                >
-                  <DeleteIcon />
-                </i>
-              </div>
-            )
-          )
+          wishList.map((element) => (
+            <WishListElement wishListElement={element} />
+          ))
         ) : (
           <div className="empty-wishlist-section">
             <span className="empty-wishlist">{wishListPage.message}</span>
